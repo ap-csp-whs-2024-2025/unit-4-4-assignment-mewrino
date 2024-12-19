a8d2600c7fb680a734cedbd8488c80d95d2de0a9
mewrino <michellenguyen04272008@gmail.com>
2024-12-19 21:26:34 +0000

    problem 2

--- homework.cpp
+++ homework.cpp
@@ -7,7 +7,7 @@ int main()
     
     // Problem 1
     int number; 
-    std::cout << "enter a number from 1 - 100: "
+    std::cout << "enter a number from 1 - 100: ";
     std::cin >> number;
 
     while (number < 1 || number > 100)
@@ -17,7 +17,21 @@ int main()
     }
     std::cout << "thank you for your input" << std::endl;
     // Problem 2
+    std::string favoriteColor = "purple";
+    std::string guess;
+    int attempts = 1;
+
+    std::cout << "guess my favorite color: ";
+    std::cin >> guess;
+
+    while (guess != favoriteColor)
+    {
+        std::cout << "incorrect! try again: ";
+        std::cin >> guess;
+        attempts = attempts + 1;
+    }
     
+    std::cout << "correct! you guessed my favorite color in " << attempts << "attempt(s)." << std::endl;
     
     // Problem 3
     
