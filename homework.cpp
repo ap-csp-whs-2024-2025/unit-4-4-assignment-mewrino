a190978270bc20d5da68e4ce9b8c1137d80a6942
mewrino <michellenguyen04272008@gmail.com>
2024-12-19 21:16:32 +0000

    problem 1

--- homework.cpp
+++ homework.cpp
@@ -6,8 +6,16 @@ int main()
     // Complete the homework problems here
     
     // Problem 1
-    
-    
+    int number; 
+    std::cout << "enter a number from 1 - 100: "
+    std::cin >> number;
+
+    while (number < 1 || number > 100)
+    {
+        std::cout << "number entered is invalid, please enter a number from 1 - 100: ";
+        std::cin >> number;
+    }
+    std::cout << "thank you for your input" << std::endl;
     // Problem 2
     
     
